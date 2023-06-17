7b1008aef89067a446854ce4a92133c58cc353ff
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-06-17 15:00:19 +0300

    feat: install react-router-dom and chakra-ui

---
+++ note.md
@@ -0,0 +1,16 @@
+### Stack: 
+
+React,
+React-router-dom
+Chackra-UI
+
+
+### Макет:
+
+https://www.figma.com/file/39F4ytcKKMLwh5RGZB3IlR/project-~15500-17200?type=design&node-id=35-1948&t=lr65VMRkiPx6wS3L-0%D1%8D
+
+Приблизительно ТЗ:
+
+https://github.com/rolling-scopes-school/tasks/tree/master/tasks/online-store-team
+
+
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Box, Button, Flex, Text, Heading, Stack } from "@chakra-ui/react"
+
+const Header = () => {
+  return (
+    <>
+      <div>LOGISTICS X</div>
+
+      <div>
+        <ul>
+          <li>Главная</li>
+          <li>Каталог</li>
+          <li>Оплата и доставки</li>
+          <li>Контакты</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Header
