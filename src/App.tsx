450703c4032185410713143d3921dec265abf69b
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-06-19 15:21:37 +0300

    feat: add style for header

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Header from './components/header/Header';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+    </>
   );
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import style from './Header.module.scss';
+
+import Bell from '../../assets/svg/bell.svg';
+import Message from '../../assets/svg/message.svg';
 
 function Header() {
   return (
-    <>
-      <div>LOGISTICS X</div>
+    <header className={style.header}>
+      <div className={style.wrapper}>
+        <div className={style.headerWrapper}>
+          <div className={style.logotype}>LOGISTICS X</div>
 
-      <div>
-        <ul>
-          <li>Главная</li>
-          <li>Каталог</li>
-          <li>Оплата и доставки</li>
-          <li>Контакты</li>
-        </ul>
+          <nav className={style.list}>
+            <ul className={style.navigation}>
+              <li>Главная</li>
+              <li>Каталог</li>
+              <li>Оплата и доставки</li>
+              <li>Контакты</li>
+            </ul>
+          </nav>
+
+          <img className={style.icon} src={Bell} alt="Колокол" />
+          <img className={style.icon} src={Message} alt="Сообщение" />
+
+          <p>
+            <button className={style.signIn}>Личный кабинет</button>
+          </p>
+        </div>
       </div>
-    </>
+    </header>
   );
 }
 
